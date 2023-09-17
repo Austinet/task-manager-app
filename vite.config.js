2c646369aaa7fce55d94f4525c903ed96ef26e86
Austinet <austinetsplendour@gmail.com>
2023-09-17 19:29:25 +0100

    bg: add the current homepage url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/task-management-app/",
+  base: "/task-manager-app/",
   plugins: [react()],
 })
